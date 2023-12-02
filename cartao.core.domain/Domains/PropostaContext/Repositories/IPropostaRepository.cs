a89dbb280ff3e2a94ae8d19cb777125b2504a3eb
adrianojcgit <48298141+adrianojcgit@users.noreply.github.com>
2023-12-02 10:18:23 -0300

    teste 4

---
+++ cartao.core.domain/Domains/PropostaContext/Repositories/IPropostaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Cartao.Domain.Domains.PropostaContext.Dto;
+
+namespace Cartao.Domain.Domains.PropostaContext.Repositories
+{
+    public interface IPropostaRepository
+    {
+        void Adicionar(PropostaBaseDto propostaBaseDto);
+    }
+}
